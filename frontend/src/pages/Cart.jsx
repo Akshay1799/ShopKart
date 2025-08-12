d557ee407ced9ff6311f2adb012a15fe4e7fda92
Akshay Ladne <Kumawata283@gmail.com>
2025-08-12 18:18:49 +0530

    added cart logic

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -19,9 +19,6 @@ const Cart = () => {
     }
 
     return (
-        <div className="container mx-auto px-4 py-8 ">
-            <h1 className="text-3xl font-bold text-center mb-8">Your Cart</h1>
-
             <div className="container mx-auto max-w-4xl px-4 py-8">
                 <h1 className="text-3xl font-bold text-center mb-8">Your Shopping Cart</h1>
 
@@ -68,8 +65,6 @@ const Cart = () => {
                     </button>
                 </div>
             </div>
-            
-        </div>
     )
 }
 
